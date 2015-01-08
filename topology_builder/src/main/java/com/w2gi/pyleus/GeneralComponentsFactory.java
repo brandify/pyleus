5e8474846ee7989f1d0dcf5d15bb93b779be961a
Ian McCall <imccall@where2getit.com>
2015-01-08 09:20:25 -0600

    Use options with non python bolts. Use more options in local mode.

--- topology_builder/src/main/java/com/w2gi/pyleus/GeneralComponentsFactory.java
+++ topology_builder/src/main/java/com/w2gi/pyleus/GeneralComponentsFactory.java
@@ -16,9 +16,21 @@ public class GeneralComponentsFactory {
     private String[] buildCommand(final String type, final String module, final Map<String, Object> argumentsMap,
             final String loggingConfig, final String serializerConfig) {
         //List<String> command = new ArrayList<String>();
-        String[] command = { type, module };
+        //String[] command = { type, module };
+        String[] command = new String[3];
         //command.add(type);
         //command.add(module);
+        command[0] = type;
+        command[1] = module;
+        
+        if (argumentsMap != null) {
+            Gson gson = new GsonBuilder().create();
+            String json = gson.toJson(argumentsMap);
+            json = json.replace("\"", "\\\"");
+            command[2] = String.format(" --options \"%s\"", json);
+        } else {
+            command[2] = "";
+        }
         
         //return command.toArray(new String[1]);
         return command;
